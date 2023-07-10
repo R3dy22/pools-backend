f6708d1f5dc882045a6da28ff9827981b1c8c3bc
R3dy22 <irinaghidenco@gmail.com>
2023-07-10 22:27:47 +0300

    upload

--- ltmpools/settings.py
+++ ltmpools/settings.py
@@ -83,7 +83,12 @@ WSGI_APPLICATION = 'ltmpools.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresql',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
